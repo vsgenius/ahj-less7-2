cabf0ae107eaa9b5bdff22395f4c83131238f3e0
Vladimir Belich <belichvladimir@mail.ru>
2023-06-21 01:13:01 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,22 @@
-document.addEventListener("DOMContentLoaded", (e) => {
+import ItcModal from "./modal";
+let currentId;
+const modal = new ItcModal({
+  content: "",
+  footerButtons: [
+    { class: "btn btn-cancel", text: "Отмена", action: "cancel" },
+    { class: "btn btn-ok", text: "Изменить", action: "ok" },
+  ],
+});
+function renderList() {
+  const myUL = document.getElementById("myUL");
+  myUL.innerHTML = "";
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState !== 4) return;
-    const res = JSON.parse(xhr.responseText);
+    let res = JSON.parse(xhr.responseText);
     for (let key in res) {
       if (res.hasOwnProperty(key)) {
-        var data = res[key];
+        let data = res[key];
         let li = document.createElement("li");
         let value = data.value;
         let t = document.createTextNode(value);
@@ -13,12 +24,52 @@ document.addEventListener("DOMContentLoaded", (e) => {
         if (data.status === "done") {
           li.className = "checked";
         }
+        let spanFind = document.createElement("SPAN");
+        spanFind.className = "find";
+        spanFind.innerHTML = "&#128269";
+        spanFind.addEventListener("click", (e) => {
+          const id = e.target.parentNode.id;
+          showText(e, id);
+        });
+        li.appendChild(spanFind);
+        let spanEdit = document.createElement("SPAN");
+        spanEdit.className = "edit";
+        spanEdit.innerHTML = "&#9998";
+        spanEdit.addEventListener("click", (e) => {
+          const xhr = new XMLHttpRequest();
+          xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) return;
+            const res = JSON.parse(xhr.responseText);
+            console.log(res);
+            modal.setBody(
+              `<div><input/ id="${
+                e.target.parentNode.id
+              }"class="value" value="${
+                res.value
+              }"><input/ class="text" value="${res.text || ""}"></div>`
+            );
+          };
+          xhr.open(
+            "GET",
+            "http://localhost:7070/?id=" + e.target.parentNode.id
+          );
+          xhr.setRequestHeader(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+          );
+          xhr.send();
+
+          modal.setTitle("Редактирование заметки");
+          modal.show();
+        });
+        li.appendChild(spanEdit);
         let span = document.createElement("SPAN");
         let txt = document.createTextNode("\u00D7");
         span.className = "close";
         span.appendChild(txt);
         li.id = key;
-        span.addEventListener('click', (e)=> {
+        currentId = key;
+        span.addEventListener("click", (e) => {
           const id = e.target.parentElement.id;
           const xhr = new XMLHttpRequest();
           xhr.onreadystatechange = function () {
@@ -35,17 +86,20 @@ document.addEventListener("DOMContentLoaded", (e) => {
           xhr.send();
         });
         li.appendChild(span);
-        document.getElementById("myUL").appendChild(li);
+        myUL.appendChild(li);
       }
     }
   };
   xhr.open("GET", "http://localhost:7070");
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.send();
+}
+document.addEventListener("DOMContentLoaded", (e) => {
+  renderList();
 });
 const subscribeForm = document.querySelector(".subscribe-form");
 let myNodelist = document.getElementsByTagName("LI");
-for (i = 0; i < myNodelist.length; i++) {
+for (let i = 0; i < myNodelist.length; i++) {
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
   span.className = "close";
@@ -53,7 +107,7 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-let list = document.querySelector("ul");
+const list = document.querySelector("ul");
 list.addEventListener(
   "click",
   function (ev) {
@@ -70,7 +124,39 @@ list.addEventListener(
   },
   false
 );
-
+const addBtn = document.querySelector(".addBtn");
+addBtn.addEventListener("click", (e) => {
+  if (subscribeForm.myInput.value === "") return;
+  const body = "value=" + e.target.parentNode.querySelector("#myInput").value;
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return;
+    newElement(xhr.response);
+  };
+  xhr.open("POST", "http://localhost:7070");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(body);
+});
+const btnCancel = document.querySelector(".btn-cancel");
+btnCancel.addEventListener("click", () => {
+  modal.hide();
+});
+const btnOk = document.querySelector(".btn-ok");
+btnOk.addEventListener("click", (e) => {
+  const value = e.target.parentNode.parentNode.querySelector(".value");
+  const text = e.target.parentNode.parentNode.querySelector(".text") || "";
+  console.log(text)
+  const body = "value=" + value.value + "&text=" + text.value;
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return;
+    renderList();
+    modal.hide();
+  };
+  xhr.open("PATCH", "http://localhost:7070/?id=" + value.id);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(body);
+});
 subscribeForm.addEventListener("submit", (e) => {
   e.preventDefault();
   if (subscribeForm.myInput.value === "") return;
@@ -84,6 +170,26 @@ subscribeForm.addEventListener("submit", (e) => {
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.send(body);
 });
+function showText(e, id) {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return;
+    const res = JSON.parse(xhr.responseText);
+    if (!e.target.parentNode.querySelector(".text")) {
+      const div = document.createElement("div");
+      div.classList.add("text");
+      div.textContent = res[id].text || "Нет текста заметки";
+      e.target.parentNode.appendChild(div);
+    } else {
+      e.target.parentElement.removeChild(
+        e.target.parentNode.querySelector(".text")
+      );
+    }
+  };
+  xhr.open("GET", "http://localhost:7070");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send();
+}
 function newElement(id) {
   let li = document.createElement("li");
   let inputValue = document.getElementById("myInput").value;
@@ -95,15 +201,44 @@ function newElement(id) {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
-  let span = document.createElement("SPAN");
+  let spanFind = document.createElement("SPAN");
+  spanFind.className = "find";
+  spanFind.innerHTML = "&#128269";
+  spanFind.addEventListener("click", (e) => {
+    const id = e.target.parentNode.id;
+    showText(e, id);
+  });
+  li.appendChild(spanFind);
+  let spanEdit = document.createElement("SPAN");
+  spanEdit.className = "edit";
+  spanEdit.innerHTML = "&#9998";
+  spanEdit.addEventListener("click", (e) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+      const res = JSON.parse(xhr.responseText);
+      modal.setBody(
+        `<div><input/ id="${e.target.parentNode.id}"class="value" value="${
+          res.value
+        }"><input/ class="text" value="${res.text || ""}"></div>`
+      );
+    };
+    xhr.open("GET", "http://localhost:7070/?id=" + e.target.parentNode.id);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send();
+
+    modal.setTitle("Редактирование заметки");
+    modal.show();
+  });
+  li.appendChild(spanEdit);
+  let spanDelete = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  span.addEventListener('click', (e)=> {
-    const id = e.target.parentElement.id;
-      e.target.parentElement.parentElement.removeChild(
-        e.target.parentElement
-      )});
-  li.appendChild(span);
+  spanDelete.className = "close";
+  spanDelete.appendChild(txt);
+  spanDelete.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+  });
+  li.appendChild(spanDelete);
   li.id = id;
+  currentId = id;
 }
